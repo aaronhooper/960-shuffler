c749fb98605eef7ed3cee490a68d494ef3bf2627
Aaron Hooper <rnjckhpr@gmail.com>
2022-04-07 21:20:34 +0100

    Refactor tests

--- test/960.test.js
+++ test/960.test.js
@@ -1,14 +1,19 @@
 const { expect } = require('chai')
 const { Position } = require('../src/960')
 
-describe('Position', () => {
-  it('throws a RangeError on inputs larger than 959 or smaller than 0', () => {
-    expect(() => new Position(960)).to.throw(RangeError)
-    expect(() => new Position(-1)).to.throw(RangeError)
+context('Position', () => {
+  describe('constructor()', () => {
+    it('throws a RangeError on inputs larger than 959 or smaller than 0', () => {
+      expect(() => new Position(960)).to.throw(RangeError)
+      expect(() => new Position(-1)).to.throw(RangeError)
+    })
   })
-  it('has the correct position', () => {
-    expect(new Position(0).getPieceArray()).to.eql(['B', 'B', 'Q', 'N', 'N', 'R', 'K', 'R'])
-    expect(new Position(518).getPieceArray()).to.eql(['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'])
-    expect(new Position(959).getPieceArray()).to.eql(['R', 'K', 'R', 'N', 'N', 'Q', 'B', 'B'])
+  describe('getPieceArray()', () => {
+    it('returns the correct position', () => {
+      expect(new Position(0).getPieceArray()).to.eql(['B', 'B', 'Q', 'N', 'N', 'R', 'K', 'R'])
+      expect(new Position(518).getPieceArray()).to.eql(['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'])
+      expect(new Position(959).getPieceArray()).to.eql(['R', 'K', 'R', 'N', 'N', 'Q', 'B', 'B'])
+    })
+
   })
 })
