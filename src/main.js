1cd9b640d109c11f714da7115be422ef831404bb
Aaron Hooper <rnjckhpr@gmail.com>
2022-04-11 16:14:48 +0100

    Add chessboard for pieces

--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
 import Position from './960.js'
 
-const whitePreElement = document.getElementById('white-pieces')
-const blackPreElement = document.getElementById('black-pieces')
 const buttonElement = document.getElementById('button')
 const positionIdElement = document.getElementById('position-id')
 
+function clearContents (element) {
+  element.innerHTML = ''
+}
+
 function refreshView () {
   const position = new Position(Position.getRandomId())
-  blackPreElement.innerHTML = position.toEmoji('black').join('')
-  whitePreElement.innerHTML = position.toEmoji('white').join('')
+  const blackEmoji = position.toEmoji('black')
+  const whiteEmoji = position.toEmoji('white')
+
+  blackEmoji.forEach((piece, index) => {
+    const square = document.querySelector(`#black-row > .board-square:nth-child(${index + 1})`)
+    clearContents(square)
+    const pieceTextNode = document.createTextNode(piece)
+    square.append(pieceTextNode)
+  })
+
+  whiteEmoji.forEach((piece, index) => {
+    const square = document.querySelector(`#white-row > .board-square:nth-child(${index + 1})`)
+    clearContents(square)
+    const pieceTextNode = document.createTextNode(piece)
+    square.append(pieceTextNode)
+  })
+
   positionIdElement.innerHTML = position.id
 }
 
