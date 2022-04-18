ba326cc089aefaa7c4f165c2c156a3f1fd699e60
Aaron Hooper <rnjckhpr@gmail.com>
2022-04-18 22:16:30 +0100

    Use react for UI

--- src/main.js
+++ src/main.js
@@ -1,34 +1,8 @@
-import Position from './960.js'
+import { createRoot } from 'react-dom/client'
+import { createElement } from 'react'
+import App from './App.js'
 import './style.css'
 
-const buttonElement = document.getElementById('button')
-const positionIdElement = document.getElementById('position-id')
-
-function clearContents (element) {
-  element.innerHTML = ''
-}
-
-function refreshView () {
-  const position = new Position(Position.getRandomId())
-  const blackEmoji = position.toEmoji('black')
-  const whiteEmoji = position.toEmoji('white')
-
-  blackEmoji.forEach((piece, index) => {
-    const square = document.querySelector(`#black-row > .board-square:nth-child(${index + 1})`)
-    clearContents(square)
-    const pieceTextNode = document.createTextNode(piece)
-    square.append(pieceTextNode)
-  })
-
-  whiteEmoji.forEach((piece, index) => {
-    const square = document.querySelector(`#white-row > .board-square:nth-child(${index + 1})`)
-    clearContents(square)
-    const pieceTextNode = document.createTextNode(piece)
-    square.append(pieceTextNode)
-  })
-
-  positionIdElement.innerHTML = position.id
-}
-
-document.addEventListener('DOMContentLoaded', () => refreshView())
-buttonElement.addEventListener('click', () => refreshView())
+const root = createRoot(document.getElementById('app'))
+const app = createElement(App, null, null)
+root.render(app)
